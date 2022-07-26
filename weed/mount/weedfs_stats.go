9f8c6cf0568983eab84484e1400ee7ee9c946703
ningfd <fangdi.ning@seetatech.com>
2022-07-26 18:18:55 +0800

    fix df -h display

--- weed/mount/weedfs_stats.go
+++ weed/mount/weedfs_stats.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/chrislusf/seaweedfs/weed/glog"
 	"github.com/chrislusf/seaweedfs/weed/pb/filer_pb"
+	"github.com/chrislusf/seaweedfs/weed/util"
 	"github.com/hanwen/go-fuse/v2/fuse"
 	"math"
 	"time"
@@ -21,56 +22,52 @@ func (wfs *WFS) StatFs(cancel <-chan struct{}, in *fuse.InHeader, out *fuse.Stat
 
 	glog.V(4).Infof("reading fs stats of %d", in.NodeId)
 
-	if in.NodeId != 1 {
-		p, _, _, status := wfs.maybeReadEntry(in.NodeId)
-		exist, rootDir := p.GetRootDir()
-		if status == fuse.OK && exist {
-
-			cachedRootEntry, cacheErr := wfs.metaCache.FindEntry(context.Background(), rootDir)
-			glog.V(4).Infof("reading fs stats of %s， cache is nil: %v", rootDir, cachedRootEntry == nil)
-			if cacheErr == filer_pb.ErrNotFound {
-				return fuse.OK
-			}
-
-			totalDiskSize := cachedRootEntry.GetXAttrSizeQuota()
-			usedDiskSize := cachedRootEntry.GetXAttrSize()
-			totalFileCount := cachedRootEntry.GetXAttrInodeQuota()
-			actualFileCount := cachedRootEntry.GetXAttrInodeCount()
-
-			glog.V(4).Infof("reading fs stats value: totalDiskSize %v ", totalDiskSize)
-			glog.V(4).Infof("reading fs stats value: usedDiskSize %v ", usedDiskSize)
-			glog.V(4).Infof("reading fs stats value: totalFileCount %v ", totalFileCount)
-			glog.V(4).Infof("reading fs stats value: actualFileCount %v ", actualFileCount)
-
-			// 超过上限了, 显示上限
-			if usedDiskSize > totalDiskSize {
-				totalDiskSize = usedDiskSize
-			}
-			if actualFileCount > totalFileCount {
-				totalFileCount = actualFileCount
-			}
-
-			// Compute the total number of available blocks
-			out.Blocks = totalDiskSize / blockSize
-
-			// Compute the number of used blocks
-			numBlocks := uint64(usedDiskSize / blockSize)
-
-			// Report the number of free and available blocks for the block size
-			out.Bfree = out.Blocks - numBlocks
-			out.Bavail = out.Blocks - numBlocks
-			out.Bsize = uint32(blockSize)
-
-			// Report the total number of possible files in the file system (and those free)
-			out.Files = totalFileCount
-			out.Ffree = totalFileCount - actualFileCount
-
-			// Report the maximum length of a name and the minimum fragment size
-			out.NameLen = 1024
-			out.Frsize = uint32(blockSize)
-
+	rootDir := util.FullPath(wfs.option.FilerMountRootPath)
+	if rootDir.IsQuotaRootNode() {
+		cachedRootEntry, cacheErr := wfs.metaCache.FindEntry(context.Background(), rootDir)
+		glog.V(4).Infof("reading fs stats of %s， cache is nil: %v", rootDir, cachedRootEntry == nil)
+		if cacheErr == filer_pb.ErrNotFound {
 			return fuse.OK
 		}
+
+		totalDiskSize := cachedRootEntry.GetXAttrSizeQuota()
+		usedDiskSize := cachedRootEntry.GetXAttrSize()
+		totalFileCount := cachedRootEntry.GetXAttrInodeQuota()
+		actualFileCount := cachedRootEntry.GetXAttrInodeCount()
+
+		glog.V(4).Infof("reading fs stats value: totalDiskSize %v ", totalDiskSize)
+		glog.V(4).Infof("reading fs stats value: usedDiskSize %v ", usedDiskSize)
+		glog.V(4).Infof("reading fs stats value: totalFileCount %v ", totalFileCount)
+		glog.V(4).Infof("reading fs stats value: actualFileCount %v ", actualFileCount)
+
+		// 超过上限了, 显示上限
+		if usedDiskSize > totalDiskSize {
+			totalDiskSize = usedDiskSize
+		}
+		if actualFileCount > totalFileCount {
+			totalFileCount = actualFileCount
+		}
+
+		// Compute the total number of available blocks
+		out.Blocks = totalDiskSize / blockSize
+
+		// Compute the number of used blocks
+		numBlocks := uint64(usedDiskSize / blockSize)
+
+		// Report the number of free and available blocks for the block size
+		out.Bfree = out.Blocks - numBlocks
+		out.Bavail = out.Blocks - numBlocks
+		out.Bsize = uint32(blockSize)
+
+		// Report the total number of possible files in the file system (and those free)
+		out.Files = totalFileCount
+		out.Ffree = totalFileCount - actualFileCount
+
+		// Report the maximum length of a name and the minimum fragment size
+		out.NameLen = 1024
+		out.Frsize = uint32(blockSize)
+
+		return fuse.OK
 	}
 
 	if wfs.stats.lastChecked < time.Now().Unix()-20 {
